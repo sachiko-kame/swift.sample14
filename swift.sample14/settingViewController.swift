15317a0e33da53b9021b42bc892fc0a80519e01e
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2017-08-03 23:38:19 +0900

    セクションなどオシャレにする。

--- swift.sample14/settingViewController.swift
+++ swift.sample14/settingViewController.swift
@@ -14,6 +14,7 @@ class settingViewController: UIViewController, UITableViewDelegate, UITableViewD
 
     
     private let myItems: NSArray = ["個人情報の設定", "ログアウト", "退会","利用規約","お問い合わせ"]
+    private let SecItems: NSArray = ["設定1", "設定2"]
     private var myTableView: UITableView!
     
     override func viewDidLoad() {
@@ -73,6 +74,11 @@ class settingViewController: UIViewController, UITableViewDelegate, UITableViewD
             print("2")
         default:
             print("3")
+            let alert = UIAlertController(title: "タイトル", message: "sampleです。", preferredStyle: .alert)
+            let buttonOk = UIAlertAction(title: "了解", style: .default, handler: nil)
+            alert.addAction(buttonOk)
+            present(alert, animated: true, completion: nil)
+
         }
         
 
@@ -86,10 +92,54 @@ class settingViewController: UIViewController, UITableViewDelegate, UITableViewD
         // 再利用するCellを取得する.
         let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath as IndexPath)
         
+        // 文字色変更
+        cell.textLabel?.textColor = UIColor.darkGray
+        // 文字サイズ変更
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 20)
+        
+        //セルのアクセサリを設定
+        cell.accessoryType = .disclosureIndicator
+        
         // Cellに値を設定する.
         cell.textLabel!.text = "\(myItems[indexPath.row])"
         
         return cell
     }
+    
+    /*
+     セルの高さを設定
+     */
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    /*
+     * セクションの数を指定します．
+     */
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return SecItems.count
+    }
+    
+    /*
+    各セクションのヘッダに表示する文字列を設定します．
+    アルファベットは全て大文字に変換されるようです．
+     */
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "\(SecItems[section])"
+    }
+    
+    /*
+    各セクションのフッタに表示する文字列を設定します．
+    こちらはアルファベットの大文字変換はされないようです．
+     */
+//    func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+//        return "footer for section: \(section)"
+//    }
+    
+    
+//    func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
+//        // アクセサリボタン（セルの右にあるボタン）がタップされた時の処理
+//        print("タップされたアクセサリがあるセルのindex番号: \(indexPath.row)")
+//    }
     
 }
