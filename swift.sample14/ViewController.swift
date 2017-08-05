c5b2f5540a7b7d242c72d89be0c1ac9f0ee572cc
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2017-08-05 13:57:29 +0900

    初期画面の設定を行う

--- swift.sample14/ViewController.swift
+++ swift.sample14/ViewController.swift
@@ -8,11 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     var addBtn: UIBarButtonItem!
+    
+    private let myItems: NSArray = ["sample1", "sample2", "sample3"]
+    private let SecItems: NSArray = ["セクション1", "セクション2"]
+    private var myTableView: UITableView!
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.title = "初期画面"
+        
+        myTableView = UITableView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height))
+        myTableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+        
+        myTableView.dataSource = self
+        
+        myTableView.delegate = self
+        
+        let viewA = UIView()
+        myTableView.tableFooterView = viewA
+        
+        self.view.addSubview(myTableView)
+        
+
         
         addBtn = UIBarButtonItem()
         addBtn.image = UIImage(named: "set1.png")?.withRenderingMode(UIImageRenderingMode.alwaysOriginal)
@@ -20,8 +42,7 @@ class ViewController: UIViewController {
         addBtn.action = #selector(ViewController.onClick)
         addBtn.target = self
         
-//        addBtn = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(ViewController.onClick))
-           self.navigationItem.leftBarButtonItem = addBtn
+        self.navigationItem.leftBarButtonItem = addBtn
 
         // Do any additional setup after loading the view.
     }
@@ -32,23 +53,78 @@ class ViewController: UIViewController {
     }
     
     func onClick() {
-//     let second = settingViewController()
-//     self.present(second, animated: true, completion: nil)
-        
         let second = settingViewController()
         let navi = UINavigationController(rootViewController: second)
         present(navi, animated: true, completion: nil)
     }
     
-
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+     Cellの総数を返すデータソースメソッド.
+     */
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return myItems.count
     }
-    */
-
+    
+    //タップされた時に呼ばれる
+    func tableView(_ table: UITableView, didSelectRowAt indexPath:IndexPath) {
+        table.deselectRow(at: indexPath, animated: true)
+        switch  indexPath.row{
+        case 0:
+            print("0")
+        case 1:
+            print("1")
+        case 2:
+            print("2")
+        default:
+            print("3")
+        }
+        
+        
+    }
+    
+    /*
+     Cellに値を設定するデータソースメソッド.
+     */
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // 再利用するCellを取得する.
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath as IndexPath)
+        
+        // 文字色変更
+        cell.textLabel?.textColor = UIColor.darkGray
+        // 文字サイズ変更
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 20)
+        
+        //セルのアクセサリを設定
+        cell.accessoryType = .disclosureIndicator
+        
+        // Cellに値を設定する.
+        cell.textLabel!.text = "\(myItems[indexPath.row])"
+        
+        return cell
+    }
+    
+    /*
+     セルの高さを設定
+     */
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    /*
+     * セクションの数を指定します．
+     */
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return SecItems.count
+    }
+    
+    /*
+     各セクションのヘッダに表示する文字列を設定します．
+     アルファベットは全て大文字に変換されるようです．
+     */
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "\(SecItems[section])"
+    }
+    
 }
