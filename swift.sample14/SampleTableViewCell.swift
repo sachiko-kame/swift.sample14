1782ff645978caa6076dffe9d56c7b8c6f671da1
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2017-08-05 15:42:06 +0900

    カスタムしていないカスタムセルを作成する

---
+++ swift.sample14/SampleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SampleTableViewCell.swift
+//  swift.sample14
+//
+//  Created by 水野祥子 on 2017/08/05.
+//  Copyright © 2017年 sachiko. All rights reserved.
+//
+
+import UIKit
+
+class SampleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
